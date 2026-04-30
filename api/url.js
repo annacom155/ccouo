fd2bd4043d7b41ce9b5b561ad25d444921cea022
annacom155 <q2020770@outlook.com>
2026-04-30 09:04:20 +0800

    Delete url.js

--- api/url.js
+++
@@ -1,9 +0,0 @@
-const http=require('http'),https=require('https')
-http.createServer((r,s)=>{
-  let p=r.url.slice(1)
-  if(p){
-    https.get(`https://raw.githubusercontent.com/annacom155/ccouo/main/${p}`,g=>{
-      g.pipe(s)
-    }).on('error',e=>s.end('Error'))
-  }else s.end('Need file path')
-}).listen(3000)
